9d58379c9ad642c04c521438cf2a817817d982c7
Amal Jama <amaljama@Amals-MacBook-Pro.local>
2020-10-20 11:15:51 -0500

    updated

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ app.jinja_env.auto_reload = True
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = 'ABC'
-
+# getting my list of most loved melonsss
 MOST_LOVED_MELONS = {
     'cren': {
         'img': 'http://www.rareseeds.com/assets/1/14/DimRegular/crenshaw.jpg',
@@ -42,7 +42,27 @@ MOST_LOVED_MELONS = {
 
 
 # REPLACE THIS WITH YOUR ROUTES
+# goes to homepage
+@app.route('/')
+def show_homepage():
+    if 'name' in session:
+        return redirect('/top-melons')
+    return render_template('homepage.html')
 
+ @app.route('/get-name')
+ #getting name from our homepage and add it into our session
+
+def get_name():
+    name = request.args.post('name')
+    session['name'] = name
+    return redirect('/top-melons')
+
+#creating a routes that renders an html of` top-melons
+@app.route('/top-melons')
+def show_top_melons():
+    if "name" not in session:
+        return redirect('/')
+    return render_template('top_melons.html', melons=MOST_LOVED_MELONS)
 
 if __name__ == '__main__':
     # We have to set debug=True here, since it has to be True at the
